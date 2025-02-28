017721c4c526aa7367a1e42a14a769f3198d59b0
Gustavo Nascimento <gustavohn2017@gmail.com>
2025-02-28 16:07:15 -0300

    Refactor Hero component to replace placeholder image with content and links

--- src/app/_components/Hero/Hero.tsx
+++ src/app/_components/Hero/Hero.tsx
@@ -1,5 +1,6 @@
 export function Hero() {
     return (
+        /*
         <section className="relative bg-gray-800 text-white h-64 md:h-96 mb-12">
             <img src="https://placehold.co/1200x400" alt="Banner Placeholder" className="absolute inset-0 w-full h-full object-cover z-0" />
             <div className="container mx-auto relative z-10 flex flex-col md:flex-row items-center h-full px-4">
@@ -12,6 +13,39 @@ export function Hero() {
                     <img src="https://placehold.co/320x300" alt="Placeholder" className="w-1/2 md:w-auto md:absolute md:right-0 md:top-1/2 md:transform md:-translate-y-1/2" />
                 </div>
             </div>
+        </section>
+        */
+        <section className="relative bg-gray-800 text-white h-64 md:h-96 mb-12 overflow-hidden">
+
+            <div className="">
+                <article>
+                    <div>
+                        <h1 className="text-4xl font-bold mb-4 lg:text-6xl leading-10">
+                            Lions Incorporadora e Serviços
+                        </h1>
+                        <p>
+                            Todo o nosso trabalho é realizado com garantia e qualidade superior!  Oferecemos orçamentos sem compromisso.
+                        </p>
+                    </div>
+
+                    <div>
+                        <a href="#">
+                         Conheça os nossos Serviços
+                        </a>
+                    </div>
+
+                    <div>
+                        <a href="#">
+                         Consulte nossas taxas
+                        </a>
+                    </div>
+                    
+
+                   
+
+                </article>
+            </div>
+
         </section>
     );
 }
