7c50345ae3c2ffe4c59a4399c148998f7bd0f361
Gustavo <gustavohn2017@gmail.com>
2025-03-10 17:40:58 -0300

    Refactor Hero and Sobre components for improved layout and image handling

--- src/app/_components/Hero/Hero.tsx
+++ src/app/_components/Hero/Hero.tsx
@@ -1,59 +1,50 @@
 "use client";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
-
 import Image from 'next/image';
 import LC from '../../../../public/assets/LC.png';
 
-
 export function Hero() {
-    return (
-        <section className="relative bg-[#000000] text-white pb-20 md:h-90 mb-6 overflow-hidden">
-            <div className="container mx-auto pt-16 pb-16 relative z-10 flex flex-col md:flex-row items-center h-full px-4">
-                <article className="w-full md:w-1/2 relative">
-                    <div className='mt-20 md:pt-0 pt-2 relative z-20'>
-                        <h1 className="text-3xl md:text-4xl font-bold mb-4 leading-tight">
-                            Lions Incorporadora e Serviços
-                        </h1>
-                        <p className="text-lg lg:text-2xl mb-6">
-                            Todo o nosso trabalho é realizado com garantia e qualidade superior! Oferecemos orçamentos sem compromisso.
-                        </p>
-                    </div>
+  return (
+    <section className="relative bg-[#000000] text-white pb-20 md:pb-0 mb-6 overflow-hidden">
+      <div className="container mx-auto pt-4 pb-4 relative z-10 flex flex-col md:flex-row items-center h-full px-4">
+        {/* Conteúdo do lado esquerdo */}
+        <article className="w-full md:w-1/2 relative p-[9px]">
+          <div className='mt-20 md:mt-0 relative z-20'>
+            <h1 className="text-3xl md:text-4xl font-bold mb-4 leading-tight">
+              Lions Incorporadora e Serviços
+            </h1>
+            <p className="text-lg lg:text-2xl mb-6">
+              Todo o nosso trabalho é realizado com garantia e qualidade superior! Oferecemos orçamentos sem compromisso.
+            </p>
+          </div>
+          <div className="mt-12 relative z-20">
+            <a className="w-fit mt-6 bg-gray-600 text-white p-4 rounded-md" href="#">
+              Conheça os nossos Serviços!
+            </a>
+          </div>
+          <div className="mt-12 mb-6 relative z-20">
+            <a className="w-fit bg-gray-600 text-white p-4 rounded-md flex items-center" href="#">
+              <FontAwesomeIcon icon={faWhatsapp} className="mr-2 h-5 max-w-8" />
+              Contato via WhatsApp
+            </a>
+          </div>
+        </article>
 
-                    <div className="mt-12 relative z-20">
-                        <a className="w-fit mt-6 bg-gray-600 text-white p-4 rounded-md" href="#">
-                            Conheça os nossos Serviços!
-                        </a>
-                    </div>
-
-                    <div className="mt-12 mb-6 relative z-20">
-                        <a className="w-fit bg-gray-600 text-white p-4 rounded-md flex items-center" href="#">
-                            <FontAwesomeIcon icon={faWhatsapp} className="mr-2 h-5 max-w-8" />
-                            Contato via WhatsApp
-                        </a>
-                    </div>
-
-                    {/* Oculta a imagem de fundo em telas grandes (acima de 1024px) e mantém a opacidade em telas menores */}
-                    <div className="hidden md:block h-full relative">
-                        <Image
-                            src={LC}
-                            alt="Imagem de exemplo"
-                            className="object-contain opacity-30"
-                            layout="fill"
-                            sizes=""
-                        />
-                    </div>
-                </article>
-
-                <article className="w-full md:w-1/2 justify-items-end relative md:block hidden">
-                    {/* Reduz a opacidade da imagem em telas menores e restaura a opacidade normal em telas maiores */}
-                    <Image
-                        src={LC}
-                        alt="Imagem de exemplo"
-                        className="w-fill h-auto opacity-30 md:opacity-100"
-                    />
-                </article>
-            </div>
-        </section>
-    );
+        {/* Imagem do lado direito (reduzida) */}
+        <article className="w-full md:w-1/2 relative p-[9px] flex justify-end">
+          <div className="relative w-[50%] h-auto">
+            <Image
+              src={LC}
+              alt="Imagem de exemplo"
+              className="object-contain w-full h-full opacity-30 md:opacity-100"
+              layout="responsive"
+              width={250} // Reduzindo a largura da imagem
+              height={250} // Reduzindo a altura da imagem
+            />
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 }
--- src/app/_components/Sobre/Sobre.tsx
+++ src/app/_components/Sobre/Sobre.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
+import Image from 'next/image';
+import SobreImage from '../../../../public/assets/todo preto2.png';
 
 const Sobre = () => {
   return (
     <section className='mb-8 p-6 md:p-12 mx-4 md:mx-20 bg-white bg-opacity-30 backdrop-filter backdrop-blur-lg rounded-lg shadow-lg flex flex-col md:flex-row'>
-      <div className='flex-1'>
+      <div className='flex-1 justify-items-center'>
         <h1 className='text-2xl font-bold mb-4 p-3'>
-          Sobre nós
+          Sobre a Lions
         </h1>
-        <div className='text-lg text-gray-700 text-justify p-3 max-w-120'>
+        <div className='text-lg text-gray-700 text-justify p-3 pb-12 max-w-300'>
           Somos um grupo especializado em Serviços Financeiros de alta performance com o objetivo de oferecer as melhores soluções para nossos clientes.
           Sendo referência em créditos imobiliários, temos proporcionado a conquista de sonhos e projetos daqueles que depositam sua confiança em nosso trabalho.
           Nossos Valores estão atrelados a condutas profissionais como ética, transparência, comprometimento, segurança, relacionamentos benéficos, responsabilidade e parceria.
         </div>
       </div>
-      <img
-        src='https://placehold.co/300x300'
-        alt='Placeholder'
-        className='mt-4 md:mb-0 md:pb-0 md:mt-0 md:ml-4 rounded-lg shadow-lg w-full md:w-auto rover:scale-110 duration-300 '
-      />
+      <div className='flex-1 flex justify-center items-center'>
+        <Image
+          src={SobreImage}
+          alt='Placeholder'
+          width={500} // Adjust the width as needed
+          height={500} // Adjust the height as needed
+          className='w-1/2 h-auto rounded-lg'
+        />
+      </div>
     </section>
   );
 };
