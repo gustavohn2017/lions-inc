8c1f5df2a69eccdbc4c6c70bc0de406dae8c3df2
Gustavo Nascimento <gustavohn2017@gmail.com>
2025-03-10 11:39:20 -0300

    Update footer copyright text for clarity and replace placeholder images with local assets in Hero component

--- src/app/_components/Hero/Hero.tsx
+++ src/app/_components/Hero/Hero.tsx
@@ -2,10 +2,14 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
+import Image from 'next/image';
+import LC from '../../../../public/assets/LC.png';
+
+
 export function Hero() {
     return (
         <section className="relative bg-[#000000] text-white pb-20 md:h-90 mb-6 overflow-hidden">
-            <div className="container mx-auto relative z-10 flex flex-col md:flex-row items-center h-full px-4">
+            <div className="container mx-auto pt-16 pb-16 relative z-10 flex flex-col md:flex-row items-center h-full px-4">
                 <article className="w-full md:w-1/2 relative">
                     <div className='mt-20 md:pt-0 pt-2 relative z-20'>
                         <h1 className="text-3xl md:text-4xl font-bold mb-4 leading-tight">
@@ -30,21 +34,23 @@ export function Hero() {
                     </div>
 
                     {/* Oculta a imagem de fundo em telas grandes (acima de 1024px) e mantém a opacidade em telas menores */}
-                    <div className="absolute inset-0 flex items-center justify-center p-4 lg:hidden">
-                        <img
-                            src='https://placehold.co/280x300'
+                    <div className="hidden md:block h-full relative">
+                        <Image
+                            src={LC}
                             alt="Imagem de exemplo"
-                            className="w-full h-auto object-contain opacity-30"
+                            className="object-contain opacity-30"
+                            layout="fill"
+                            sizes=""
                         />
                     </div>
                 </article>
 
-                <article className="w-full md:w-1/2 relative md:block hidden">
+                <article className="w-full md:w-1/2 justify-items-end relative md:block hidden">
                     {/* Reduz a opacidade da imagem em telas menores e restaura a opacidade normal em telas maiores */}
-                    <img
-                        src='https://placehold.co/280x300'
+                    <Image
+                        src={LC}
                         alt="Imagem de exemplo"
-                        className="w-full h-auto opacity-30 md:opacity-100"
+                        className="w-fill h-auto opacity-30 md:opacity-100"
                     />
                 </article>
             </div>
