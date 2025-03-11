b3a1e91034ec927793b8f1b3a4db8b10ba06e6da
Gustavo <gustavohn2017@gmail.com>
2025-03-11 16:02:19 -0300

    Update Produtos component with new product details and improve styling for better visibility and responsiveness

--- src/app/_components/Produtos/Produtos.tsx
+++ src/app/_components/Produtos/Produtos.tsx
@@ -4,75 +4,84 @@ import React, { useState } from 'react';
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
+type Produto = {
+    titulo: string;
+    descricao: string;
+    recursos: string[];
+};
+
+type ProdutosType = {
+    [key: string]: Produto;
+};
+
 const Produtos = () => {
-    const produtos = {
+    const produtos: ProdutosType = {
         Tab1: {
-            titulo: "Produto 1",
-            descricao: "Descrição detalhada do Produto 1. Este produto oferece [recursos/benefícios].",
-            recursos: ["Recurso A", "Recurso B", "Recurso C"],
+            titulo: "Consórcios",
+            descricao: "Consórcios são uma forma de adquirir bens ou serviços por meio de autofinanciamento coletivo.",
+            recursos: ["Planejamento financeiro", "Sem juros", "Diversas opções de crédito"],
         },
         Tab2: {
-            titulo: "Produto 2",
-            descricao: "Descrição detalhada do Produto 2. Este produto é ideal para [público-alvo].",
-            recursos: ["Recurso X", "Recurso Y", "Recurso Z"],
+            titulo: "Financiamentos",
+            descricao: "Financiamentos são empréstimos concedidos para a aquisição de bens ou serviços, com pagamento parcelado.",
+            recursos: ["Taxas competitivas", "Prazos flexíveis", "Aprovação rápida"],
         },
         Tab3: {
-            titulo: "Serviço 1",
-            descricao: "Descrição detalhada do Serviço 1. Este serviço inclui [componentes/etapas].",
-            recursos: ["Etapa 1", "Etapa 2", "Etapa 3"],
+            titulo: "Capital de Giro",
+            descricao: "Capital de giro para empresas é o recurso necessário para financiar as operações diárias de um negócio.",
+            recursos: ["Melhora do fluxo de caixa", "Facilidade de acesso", "Condições personalizadas"],
         },
         Tab4: {
-            titulo: "Serviço 2",
-            descricao: "Descrição detalhada do Serviço 2. Este serviço fornece [resultados/soluções].",
-            recursos: ["Solução A", "Solução B", "Solução C"],
+            titulo: "Consultoria Financeira",
+            descricao: "Consultoria financeira empresarial oferece orientação especializada para a gestão financeira do seu negócio.",
+            recursos: ["Análise de mercado", "Planejamento estratégico", "Apoio na tomada de decisões"],
         },
     };
 
     const [selectedTab, setSelectedTab] = useState("Tab1");
 
     return (
-        <section className="p-4 md:p-20 flex flex-col items-center">
+        <section className="p-4 md:p-10 flex flex-col items-center w-screen bg-[#333]">
             <div className="text-center">
-                <h1 className="text-2xl font-bold">Produtos e Serviços</h1>
+                <h1 className="text-2xl font-bold text-white">Produtos e Serviços</h1>
             </div>
 
-            <div className="w-full md:w-[600px] mt-8">
+            <div className="w-full mt-8">
                 {/* Dropdown para telas menores */}
                 <div className="md:hidden flex justify-center">
                     <Select value={selectedTab} onValueChange={setSelectedTab}>
-                        <SelectTrigger className="w-full bg-gray-100 border rounded-md text-center">
+                        <SelectTrigger className="w-full bg-white border rounded-md text-center">
                             <SelectValue placeholder="Selecione um produto/serviço" />
                         </SelectTrigger>
-                        <SelectContent className="bg-gray-100 border rounded-md">
+                        <SelectContent className="bg-white border rounded-md">
                             {Object.keys(produtos).map((tab) => (
                                 <SelectItem key={tab} value={tab} className="flex justify-center">
-                                    {tab}
+                                    {produtos[tab].titulo}
                                 </SelectItem>
                             ))}
                         </SelectContent>
                     </Select>
                 </div>
 
-                {/* Card para telas maiores */}
-                <div className="hidden md:block bg-gray-100 rounded-lg shadow-md p-6 justify-center"> {/* Adicionado flex justify-center aqui */}
-                    <Tabs defaultValue="Tab1" className="w-full"> {/* Manteve w-full aqui */}
-                        <TabsList className="flex justify-center border-b border-gray-200 w-full"> {/* Adicionado w-full aqui */}
+                <div className="hidden md:block justify-center w-full">
+                    <Tabs defaultValue="Tab1" className="w-full">
+                        <TabsList className="relative flex w-full p-1 rounded-xl bg-zinc-800 shadow-md">
                             {Object.keys(produtos).map((tab) => (
                                 <TabsTrigger
                                     key={tab}
                                     value={tab}
-                                    className="text-sm md:text-base px-4 py-2 rounded-t-md transition-colors duration-200 hover:bg-gray-200 data-[state=active]:bg-white mx-2 text-gray-600 hover:text-gray-800 font-semibold"
+                                    className="relative w-full h-10 rounded-md text-sm font-medium whitespace-nowrap transition-all focus-visible:relative focus-visible:z-10 data-[state=active]:bg-zinc-700 data-[state=active]:text-white text-white hover:text-white"
                                 >
-                                    {tab}
+                                    {produtos[tab].titulo}
                                 </TabsTrigger>
                             ))}
                         </TabsList>
                         {Object.entries(produtos).map(([tab, conteudo]) => (
-                            <TabsContent key={tab} value={tab} className="mt-4">
+                            <TabsContent key={tab} value={tab} className="mt-4 p-6">
                                 <div className="p-2 md:p-4">
-                                    <h2 className="text-lg md:text-xl font-semibold mb-2">{conteudo.titulo}</h2>
-                                    <p className="text-gray-700 mb-4 text-sm md:text-base">{conteudo.descricao}</p>
-                                    <ul className="list-disc list-inside text-sm md:text-base">
+                                    <h2 className="text-xl font-semibold mb-2 text-white">{conteudo.titulo}</h2>
+                                    <p className="text-white mb-4 text-sm md:text-base">{conteudo.descricao}</p>
+                                    <ul className="list-disc list-inside text-sm md:text-base text-white">
                                         {conteudo.recursos.map((recurso, index) => (
                                             <li key={index}>{recurso}</li>
                                         ))}
@@ -83,15 +92,15 @@ const Produtos = () => {
                     </Tabs>
                 </div>
 
-                {/* Conteúdo dinâmico para telas menores */}
+                {/* Lista para dispositivos mobile. */}
                 <div className="md:hidden flex justify-center">
                     <div className="w-full">
                         {Object.entries(produtos).map(([tab, conteudo]) => (
                             selectedTab === tab && (
-                                <div key={tab} className="p-2 md:p-4 bg-gray-100 rounded-lg shadow-md mt-4">
-                                    <h2 className="text-lg md:text-xl font-semibold mb-2 text-center">{conteudo.titulo}</h2>
-                                    <p className="text-gray-700 mb-4 text-sm md:text-base">{conteudo.descricao}</p>
-                                    <ul className="list-disc list-inside text-sm md:text-base">
+                                <div key={tab} className="p-6 mt-4">
+                                    <h2 className="text-xl font-semibold mb-2 text-center text-white">{conteudo.titulo}</h2>
+                                    <p className="text-white mb-4 text-sm md:text-base">{conteudo.descricao}</p>
+                                    <ul className="list-disc list-inside text-sm md:text-base text-white">
                                         {conteudo.recursos.map((recurso, index) => (
                                             <li key={index}>{recurso}</li>
                                         ))}
